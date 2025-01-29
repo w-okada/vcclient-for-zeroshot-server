3e48aad6e6a5a2c0f81c3ff110f012ca1aed29e2
wok <wok@example.com>
2025-01-30 05:25:43 +0900

    test crlf

--- installer/run.bat
+++ installer/run.bat
@@ -1,11 +1,6 @@
-@REM Description: This script is used to install the VCClient for Zeroshot VC.
-@REM reference:
-@REM https://github.com/niel-blue/beatrice-trainer-webui/blob/main/setup.bat
-
 chcp 65001 > NUL
 @echo off
 
-echo.
 echo  =================================================
 echo  VCClient for Zeroshot VC 
 echo  =================================================
